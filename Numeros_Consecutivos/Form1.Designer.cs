473addd81affab7f60ac88317ec8b7465fc0cec0
Michael Escobar Galeano <maicolgemelo@gmail.com>
2019-09-14 18:53:02 -0500

    Muestra numero consecutivo desde cero hasta el numero que escoga el usuario

---
+++ Numeros_Consecutivos/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Numeros_Consecutivos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.lblMensaje = new System.Windows.Forms.Label();
+            this.lsbConsecutivo = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(275, 95);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(241, 20);
+            this.txtCantidad.TabIndex = 0;
+            this.txtCantidad.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCantidad_KeyPress);
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(145, 154);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(75, 43);
+            this.btnMostrar.TabIndex = 1;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Location = new System.Drawing.Point(334, 154);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(75, 43);
+            this.btnBorrar.TabIndex = 2;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(520, 154);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 43);
+            this.btnSalir.TabIndex = 3;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // lblMensaje
+            // 
+            this.lblMensaje.AutoSize = true;
+            this.lblMensaje.Location = new System.Drawing.Point(272, 257);
+            this.lblMensaje.Name = "lblMensaje";
+            this.lblMensaje.Size = new System.Drawing.Size(47, 13);
+            this.lblMensaje.TabIndex = 4;
+            this.lblMensaje.Text = "Mensaje";
+            this.lblMensaje.Visible = false;
+            // 
+            // lsbConsecutivo
+            // 
+            this.lsbConsecutivo.FormattingEnabled = true;
+            this.lsbConsecutivo.Location = new System.Drawing.Point(275, 291);
+            this.lsbConsecutivo.Name = "lsbConsecutivo";
+            this.lsbConsecutivo.Size = new System.Drawing.Size(201, 147);
+            this.lsbConsecutivo.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lsbConsecutivo);
+            this.Controls.Add(this.lblMensaje);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.txtCantidad);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label lblMensaje;
+        private System.Windows.Forms.ListBox lsbConsecutivo;
+    }
+}
+
---
+++ Numeros_Consecutivos/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Numeros_Consecutivos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtCantidad_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsNumber(e.KeyChar))
+            {
+
+                e.Handled = false;
+            }
+            else if (char.IsControl(e.KeyChar))
+            {
+
+                e.Handled = false;
+            }
+            else if (char.IsSeparator(e.KeyChar))
+            {
+
+                e.Handled = false;
+            }
+            else
+            {
+
+                MessageBox.Show("Solo se pueden numeros"
+                    , "Mensaje de advertencia"
+                    , MessageBoxButtons.OK
+                    , MessageBoxIcon.Error);
+
+                e.Handled = true;
+                return;
+            }
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            int cantidad = int.Parse(txtCantidad.Text);
+            int contador = 0;
+            int resta = 0;
+
+            lsbConsecutivo.Items.Clear();
+
+            while (contador <= cantidad)
+            {
+
+                if (contador <= cantidad)
+                {
+
+                    lsbConsecutivo.Items.Add(contador.ToString());
+
+                }
+
+
+                lblMensaje.Text = " Muestra Finalizada";
+                lblMensaje.Visible = true;
+                contador++;
+                txtCantidad.Text = "";
+                txtCantidad.Focus();
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            lsbConsecutivo.Items.Clear();
+        }
+    }
+}
